089e538680a19c3a8706b3a8b354048ae9595077
Noah Larsen <noah_larsen@outlook.com>
2018-09-04 17:52:29 -0500

    Squashed 'src/main/scala/utils/' changes from 3a63a6c..3155fc7
    3155fc7 updated utils
    18a41e4 updated utils
    e33a052 Merge commit '50dbfd2439d8130425c64f63af6db038f13c37fa' as 'src/main/scala/utils'
    6c10299 removed utils in preparation for adding them back in with subtree
    7bf9308 updated commands
    35d2217 added update all
    261cd96 updated Commands
    8fe0f19 Merge commit 'dc9d211eeb72849fb737e1246b750d86d2842b85' as 'src/main/scala/utils'
    d873953 deleting utils to then add as subtree
    893db60 made primary date fields more flexible
    e72489d improved commands
    3783183 initial completion of writing when the number of columns differ, various other updates
    008d159 towards updating speadsheets when row counts differ
    40abd88 towards generating or renaming primary date field
    86b419b refactored
    df0f6c7 updated utils
    f790479 updated utils
    01e1d00 updated initial data dictionary
    ed1b281 progress towards browsing related nodes
    88565b8 updated merges
    e7eb7ac updated work document transformations, improved loading and saving
    7543dbf initial completion of auto-generating generated field dictionary information
    bc1d947 progress towards auto-generating dictionary information for generated fields
    2ed6b59 added support for writing to initial data dictionary
    c82d3ed fixes and additions involving types and date formats, in process of reworking phoenix owner data dictionary
    b4f4a82 improved iterate
    c1ac772 completed initial full integration of the initial data dictionary
    63008f8 more progress towards incorporating initial data dictionary
    af0e460 continued progress on incorporating and integrating initial data dictionary
    4b7e403 added nearby node computations, extended and refactored command classes
    f583838 initial completion of initial data dictionary object and fields
    0005fae temp
    ab51103 temp
    88f97bb heavily developed the parameterization components of the command classes
    2081244 added unapproved names to the search with differentiation in the match results
    6ed549c added other
    102f301 created main commands
    b85d4c8 updated .gitignore
    3e48997 updated .gitignore
    0982bf6 added ConnectedForests and DevelopingConnectedForests json formats, updated command and enumerated utils
    64a79a4 refactored commands
    f063ffd added creation of master fields and support for writing both raw and master fields
    905e6ea added local
    3d9848b refactored Command
    c167bdd updated extraction of the logical format of work document entries from field entries
    0499c0c added support for reading the google spreadsheet modified registration date from the work data dictionary
    1d3f53a refactored commands, enumerated
    df2aff3 merged
    0c3a19d updated FieldEntryReaderWriter
    c9ec477 Merge branch 'master' of ssh://globaldevtools.bbva.com:7999/~noah.larsen/zgusta
    27d221a in process of refactoring command
    5a19764 Merge pull request #2 in ~NOAH.LARSEN/zgusta from develop to master
    d890bd9 fixed phoenix source data dictionary update
    327eb67 Merge pull request #1 in ~NOAH.LARSEN/zgusta from develop to master
    b18bbb3 began adding integer-range named command support, updated phoenix source data dictionary
    ec0a0c0 renamed command
    ffb645f added text-based searching, combination name comparators, logical format inference
    5802fb9 in process of adding json serialization
    c7eb424 various small changes
    e5e9258 deleted from repo .idea, project, and target
    15a42b6 various progress
    93a161c necessary commit for performing stash pop
    f904600 initial commit
    e0210cc added LabeledForest
    
    git-subtree-dir: src/main/scala/utils
    git-subtree-split: 3155fc75147756409035ff6d844bcba85d05f82f

--- commands/Commands.scala
+++ commands/Commands.scala
@@ -1,10 +1,10 @@
 package utils.commands
 
-import utils.IO
 import utils.commands.Parameter.{ListParameter, OptionalParameter, ValueParameter}
 import utils.commands.Commands.{CommandException, IncorrectNumberParametersException, InvalidParametersException, UnknownCommandException}
 import utils.commands.IndexedCommand.{IndexCommand, IndexListCommand}
 import utils.enumerated.Enumerated
+import utils.io.IO
 
 import scala.io.StdIn
 import scala.util.{Failure, Success, Try}
@@ -52,83 +52,22 @@ trait Commands extends Enumerated {
 
 
   private def parse[T](line: String, indexedCommandValues: Map[Int, T] = Map(), without: Seq[CommandType] = Seq()): Try[CommandInvocation[CommandType, T]] = {
-
     val whitespaceRe = "\\s+"
     val tokens = line.split(whitespaceRe)
-
-
     letterCommands
       .find(_.letterName.toString == tokens.head).filter(!without.contains(_)).map(CommandInvocation[CommandType, T](_, tokens.tail).validate)
       .orElse(
         indexedCommand.collect {
-          case _: IndexCommand =>
-            indexedCommand.filter(!without.contains(_)).map((_, Try(tokens.head.toInt))).filter(_._2.isSuccess).map(x => (x._1, x._2.get)).filter(x => indexedCommandValues
-              .contains(x._2)).map(x => CommandInvocation(x._1, tokens.tail, Some(indexedCommandValues(x._2))).validate)
-          case _: IndexListCommand =>
-            indexedCommand.filter(!without.contains(_)).map((_, tokens.map(y => Try(y.toInt)))).filter(_._2.forall(_.isSuccess)).map(x => (x._1, x._2.map(_.get))).filter(_._2
-              .forall(x => indexedCommandValues.contains(x))).map(x => Try(CommandInvocation[CommandType, T](x._1, Seq(), None, Some(x._2.map(indexedCommandValues(_))))))
+          case x if x.isInstanceOf[IndexCommand] =>
+            indexedCommand.filter(!without.contains(_)).map((_, Try(tokens.head.toInt))).filter(_._2.isSuccess).map(y => (y._1, y._2.get)).filter(y => indexedCommandValues
+              .contains(y._2)).map(y => CommandInvocation(y._1, tokens.tail, Some(indexedCommandValues(y._2))).validate)
+          case x if x.isInstanceOf[IndexListCommand] =>
+            indexedCommand.filter(!without.contains(_)).map((_, tokens.map(y => Try(y.toInt)))).filter(_._2.forall(_.isSuccess)).map(y => (y._1, y._2.map(_.get))).filter(_._2
+              .forall(y => indexedCommandValues.contains(y))).map(y => Try(CommandInvocation[CommandType, T](y._1, Seq(), None, Some(y._2.map(indexedCommandValues(_))))))
         }.flatten
       )
       .getOrElse(Failure[CommandInvocation[CommandType, T]](UnknownCommandException)).asInstanceOf[Try[CommandInvocation[CommandType, T]]]
-
   }
-
-
-//  private[utils] case class CommandInvocation[T](
-//                                   command: CommandType,
-//                                   arguments: Seq[String],
-//                                   indexCommandSelection: Option[T] = None,
-//                                   indexListCommandSelection: Option[Seq[T]] = None
-//                                 ){
-//
-//    def validate: Try[CommandInvocation[T]] = {
-//      Unit match {
-//        case _ if !(parameters.length - command.nUnrequiredParameters to parameters.collectFirst{case _: ListParameter[_] => Int.MaxValue}.getOrElse(parameters.length))
-//          .contains(arguments.length) => Failure(IncorrectNumberParametersException)
-//        case _ if parameterToArguments.exists(x => x._1 match {
-//          case y: ValueParameter[_] => y.parse(x._2.head).isFailure
-//          case y: OptionalParameter[_] => y.parse(x._2.head).isFailure
-//          case y: ListParameter[_] => y.parse(x._2).isFailure
-//        }) => Failure(InvalidParametersException)
-//        case _ => Try(this)
-//      }
-//    }
-//
-//
-//    def value[U](valueParameter: ValueParameter[U]): U = {
-//      (parameters.indexOf(valueParameter), valueParameter.default) match {
-//        case (x, Some(y)) if !arguments.indices.contains(x) => y
-//        case (x, _) => valueParameter.parse(arguments(x)).get
-//      }
-//    }
-//
-//
-//    def value[U](optionalParameter: OptionalParameter[U]): Option[U] = {
-//      Some(parameters.indexOf(optionalParameter))
-//        .filter(arguments.indices.contains)
-//        .map(x => optionalParameter.parse(arguments(x)).get)
-//    }
-//
-//
-//    def value[U](listParameter: ListParameter[U]): Seq[U] = {
-//      listParameter.parse(arguments.slice(command.parameters.indexOf(listParameter), arguments.length)).get
-//    }
-//
-//
-//    private def parameterToArguments: Map[Parameter, Seq[String]] = {
-//      parameters.lastOption.map{
-//        case x: ValueParameter[_] => parameters.zip(arguments).map(x => (x._1, Seq(x._2))).toMap
-//        case x: OptionalParameter[_] => parameters.zip(arguments).map(x => (x._1, Seq(x._2))).toMap
-//        case x: ListParameter[_] => parameters.init.zip(arguments).map(x => (x._1, Seq(x._2))).toMap + (x -> arguments.slice(parameters.length - 1, arguments.length))
-//      }.getOrElse(Map())
-//    }
-//
-//
-//    private def parameters: Seq[Parameter] = {
-//      command.parameters
-//    }
-//
-//  }
 
 }
 
