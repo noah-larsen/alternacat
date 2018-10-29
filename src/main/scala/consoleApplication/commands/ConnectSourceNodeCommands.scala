b7fe1a82daa6afc4cd955fe5143046786d767990
Noah Larsen <noah_larsen@outlook.com>
2018-10-29 00:03:07 -0500

    towards help commands

--- src/main/scala/consoleApplication/commands/ConnectSourceNodeCommands.scala
+++ src/main/scala/consoleApplication/commands/ConnectSourceNodeCommands.scala
@@ -1,6 +1,7 @@
 package consoleApplication.commands
 
 import consoleApplication.CommonParameters.{Keyword, MaxDepth}
+import consoleApplication.commands.MainCommands.MaxFinishedValue1To5
 import consoleApplication.finishedValues
 import utils.commands.Parameter.OptionalParameter
 import utils.commands._
@@ -24,16 +25,24 @@ object ConnectSourceNodeCommands extends Commands {
   object FinishedValue1To5 extends OptionalParameter(x => Try(x.toInt).filter(finishedValues.contains))
 
 
+  val relatedTargetNodesDescription = "Displays the nodes from the target forest that are related to this source node."
+  val goToTargetNodesDescription = "Moves to the target forest, which can be traversed and edited, including setting which target nodes are related to the source node under " +
+    "consideration here."
+  val searchTargetNodesDescription = "Performs a text-based search over the names of nodes in the target forest. If applicable, matching nodes can be traversed to and set " +
+    "as related to the source node under consideration here."
+  val descendantsHelpDescription = s"Displays the nodes underneath this node in the source forest, up to ${MaxDepth.name} levels down."
+
+
   override protected def help: (ConnectSourceNodeCommand) => String = {
-    case RelatedTargetNodes => "Displays the nodes from the target forest that are related to this source node."
-    case GoToTargetNodes =>
-      "Moves to the target forest, which can be traversed and edited, including setting which target nodes are related to the source node under consideration here."
-    case SearchTargetNodes =>
-      "Performs a text-based search over the names of nodes in the target forest. If applicable, matching nodes can be traversed to and set as related to the source node " +
-        "under consideration here."
-    case Descendants => s"Displays the nodes underneath this node in the source forest, up to ${MaxDepth.name} levels down."
-    case Kin => ""
-    case Next => ""
+    case RelatedTargetNodes => relatedTargetNodesDescription
+    case GoToTargetNodes => goToTargetNodesDescription
+    case SearchTargetNodes => searchTargetNodesDescription
+    case Descendants => descendantsHelpDescription
+    case Kin => s"Displays the node's siblings and their ancestors, up to ${MaxDepth.name} levels up."
+    case Next =>
+      s"Moves to the next source node available from the source nodes selected for processing and their descendants. The node's finished value is set to ${
+        FinishedValue1To5.name} if specified, and retains its current value otherwise. For a given ${MaxFinishedValue1To5.name}, descendants of this source node cannot be " +
+        s"processed until a higher ${FinishedValue1To5.name} is set for this source node."
     case Back => ""
   }
 
--- src/main/scala/consoleApplication/commands/EditRelatedNodesCommands.scala
+++ src/main/scala/consoleApplication/commands/EditRelatedNodesCommands.scala
@@ -6,19 +6,19 @@ import utils.commands.{Command, Commands, Parameter}
 object EditRelatedNodesCommands extends Commands {
 
   override type CommandType = EditRelatedNodeCommand
-  sealed abstract class EditRelatedNodeCommand(parameters: Seq[Parameter] = Seq()) extends Command(parameters)
+  sealed abstract class EditRelatedNodeCommand(parameters: Seq[Parameter] = Seq(), specifiedLetterName: Option[Char] = None) extends Command(parameters, specifiedLetterName)
 
   object RelatedNodes extends EditRelatedNodeCommand
   object Descendants extends EditRelatedNodeCommand(Seq(MaxDepth))
-  object LookupTargetNodes extends EditRelatedNodeCommand
+  object GoToTargetNodes extends EditRelatedNodeCommand(specifiedLetterName = Some('t'))
   object SearchTargetNodes extends EditRelatedNodeCommand(Seq(Keyword))
   object Back extends EditRelatedNodeCommand
 
   override protected def help: (EditRelatedNodeCommand) => String = {
-    case RelatedNodes => ""
-    case Descendants => ""
-    case LookupTargetNodes => ""
-    case SearchTargetNodes => ""
+    case RelatedNodes => ConnectSourceNodeCommands.relatedTargetNodesDescription
+    case Descendants => ConnectSourceNodeCommands.descendantsHelpDescription
+    case GoToTargetNodes => ConnectSourceNodeCommands.goToTargetNodesDescription
+    case SearchTargetNodes => ConnectSourceNodeCommands.searchTargetNodesDescription
     case Back => ""
   }
 
